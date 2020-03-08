5dae27a4789d2fc72db8883b15e3e0c6b6af3a84
gabi <gabrie.rak@gmail.com>
2020-03-07 23:33:21 -0500

    completed

---
+++ data/friends.js
@@ -0,0 +1,31 @@
+//array of default friends
+
+var friends = [{
+    name: "John Dope",
+    photo: "https://static.pexels.com/photos/343717/pexels-photo-343717.jpeg",
+    scores: [3, 1, 4, 4, 2, 4, 5, 2, 4, 2]
+  }, {
+    name: "Laura Stoner",
+    photo: "https://static.pexels.com/photos/206481/pexels-photo-206481.jpeg",
+    scores: [2, 5, 3, 4, 5, 3, 1, 3, 2, 4]
+  }, {
+    name: "Bob White",
+    photo: "https://static.pexels.com/photos/64385/pexels-photo-64385.jpeg",
+    scores: [4, 3, 3, 2, 1, 2, 5, 4, 2, 1]
+  },{
+    name: "Amy Colby",
+    photo: "https://static.pexels.com/photos/270308/pexels-photo-270308.jpeg",
+    scores: [1, 2, 3, 3, 4, 1, 2, 5, 3, 3]
+  }, {
+    name: "Anna Smith",
+    photo: "https://static.pexels.com/photos/206481/pexels-photo-206481.jpeg",
+    scores: [2, 1, 1, 2, 5, 4, 3, 3, 1, 4]
+  }, {
+    name: "Jack Wallace",
+    photo: "https://static.pexels.com/photos/249760/pexels-photo-249760.jpeg",
+    scores: [2, 2, 1, 3, 4, 2, 5, 3, 3, 4]
+  }];
+  
+   
+  
+  module.exports = friends;
